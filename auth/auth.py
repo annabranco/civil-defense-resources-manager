1478652e0a5c3b8c9c0f8a2f52af611c7ca2e34f
Anna Branco <annabarau@ext.inditex.com>
2021-09-28 20:40:09 +0200

    [#33] Reconfigured API to correctly use logout callbacks

--- auth/auth.py
+++ auth/auth.py
@@ -12,14 +12,13 @@ if ENV_FILE:
     load_dotenv(ENV_FILE)
 
 AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
-AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 ALGORITHMS = env.get(constants.ALGORITHMS)
-AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
 AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
 AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-AUTH0_SCOPE = env.get(constants.AUTH0_SCOPE)
-SECRET_KEY = env.get(constants.SECRET_KEY)
+AUTH0_LOGOUT_CALLBACK_URL = env.get(constants.AUTH0_LOGOUT_CALLBACK_URL)
 
 class AuthError(Exception):
     def __init__(self, error, status_code):
